ce77c3c767c2d8458b44f0672bbe9a8034b33223
Mohamed <mohammed.elkhazen@gmail.com>
2023-12-01 13:41:17 -0600

    initial commit

---
+++ src/test/java/com/portfolio/melkhazen/chartercommunications/CharterCommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.melkhazen.chartercommunications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharterCommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
